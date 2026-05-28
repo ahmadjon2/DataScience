d2e050accb6a3a2f1c170020c504b97f932b4f68
ahmadjon2 <khamro.ruziev@gmail.com>
2026-05-28 20:04:58 +0100

    Add files via upload

--- LinearRegression.py
+++ LinearRegression.py
@@ -12,19 +12,47 @@ plt.show()
 
 mx = x.mean()
 my = y.mean()
-m = np.sum((x - mx)*(y - my) / sum(x - mx)**2)
+m = np.sum((x - mx)*(y - my)) / np.sum((x - mx)**2)
 c = my - m*mx
 
-#findding line of best fit 
+#finding line of best fit 
 
 """
 y = mx + c
 
-m = np.sum((xi - mx)(yi - my) / sum(xi - mx**2))
+m = np.sum((xi - mx)(yi - my)) / sum((xi - mx)**2)
 c = my - m*mx
 """
 
 y1 = mx + c
-print(m, c, y)
+print(m)
+print(c)
+print(y)
 
 predictionY = m * x + c
+
+plt.scatter(x,y)
+plt.plot(x,predictionY)
+plt.show()
+
+# error  -  RMSE : root mean squared error
+
+error = np.sqrt(np.mean((predictionY - y)**2))
+print(error)
+
+# using libary 
+
+from sklearn.linear_model import LinearRegression
+
+x = x.reshape(-1,1)
+lr = LinearRegression()
+lr.fit(x,y)
+print("slope",lr.coef_)
+print("intercept",lr.intercept_)
+predictionY = lr.predict(x)
+print(predictionY)
+
+from sklearn.metrics import root_mean_squared_error
+
+errorY = root_mean_squared_error(y,predictionY)
+print(errorY)
