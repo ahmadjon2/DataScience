fdbc5a01d0e9e59d9d1f62a65add1f3240cd6f6e
ahmadjon2 <khamro.ruziev@gmail.com>
2026-05-31 18:05:13 +0100

    Add files via upload

---
+++ MultiveriableRegression.py
@@ -0,0 +1,44 @@
+# y = mx + c
+# y = m1x1 + m2x2 + c
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+data = pd.read_csv("iris.csv")
+
+print(data.info())
+
+#finding no values
+
+print(data.isna().sum())
+
+X = data.iloc[:,:-1]
+y = data.iloc[:,-1]
+
+from sklearn.preprocessing import LabelEncoder
+
+le = LabelEncoder()
+le.fit(y)
+y = le.transform(y)
+print(y)
+
+from sklearn.model_selection import train_test_split
+
+Xtrain,Xtest,Ytrain,Ytest = train_test_split(X,y,train_size=0.7,random_state=10)
+
+from sklearn.linear_model import LinearRegression
+
+object = LinearRegression()
+object.fit(Xtrain,Ytrain)
+m = object.coef_
+c = object.intercept_
+Ytrainpredict = object.predict(Xtrain)
+Ytestpredict = object.predict(Xtest)
+
+from sklearn.metrics import root_mean_squared_error
+
+errorY = root_mean_squared_error(Ytrain,Ytrainpredict)
+print(errorY)
+errorTestY = root_mean_squared_error(Ytest,Ytestpredict)
+print(errorTestY)
